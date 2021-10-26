e4191399b28eb7d18b7596ede385414e568589a3
Perry <petreli1@ymail.com>
2021-10-26 11:48:38 +0100

    redsigned the check win code to color the winning line + added ability to draw + added reset board function

--- ttt.js
+++ ttt.js
@@ -1,5 +1,12 @@
 const GameBoard = (() => {
     var board = [];
+    board.length = 9;
+
+    const resetBoard = () => {
+        board = [];
+        board.length = 9;
+    };
+
     const container = document.getElementById('gameboard');
     const cell = document.querySelectorAll('.cell');
     let turn = 0;
@@ -15,6 +22,7 @@ const GameBoard = (() => {
         for (i=0; i<9; i++) {
             if (board[i] != undefined) {
                 cell[i].innerHTML = board[i];
+                cell[i].style.color = 'black'
             } else {
                 cell[i].innerHTML = '';
             }
@@ -28,29 +36,60 @@ const GameBoard = (() => {
                 board[cellIndex] = currentPlay();
                 turn++;
                 updateBoard();
-                checkWinner();
+                checkWin();
             }
         });
     });
 
-    const checkWinner = () => {
-        if (((board[4] !== undefined) &&
-            ((board[4] === board[3] && board[4] === board[5]) ||
-            (board[4] === board[1] && board[4] === board[7]) ||
-            (board[4] === board[0] && board[4] === board[8]) ||
-            (board[4] === board[6] && board[4] === board[2]) ))
-            || ((board[0] !== undefined) &&
-            ((board[0] === board[1] && board[0] === board[2]) ||
-            (board[0] === board[3] && board[0] === board[6])))
-            || ((board[8] !== undefined) &&
-            ((board[8] === board[2] && board[8] === board[5]) ||
-            (board[8] === board[7] && board[8] === board[6])))
-            ){
-                console.log("WINNER is u")
-                board = [];
-            }
-        
+    let winner = false;
+    let draw = false;
+    const checkWin = () => {
+        if (board[0] !== undefined && board[0] === board[1] && board[0] === board[2]){ //top row
+            colorLine(0, 1, 2);
+            resetBoard();
+            winner = true;
+        } else if (board[3] !== undefined && board[3] === board[4] && board[3] === board[5]){ //middle row
+            colorLine(3, 4, 5);
+            resetBoard();
+            winner = true;
+        } else if (board[6] !== undefined && board[6] === board[7] && board[6] === board[8]){ //bottom row
+            colorLine(6, 7, 8);
+            resetBoard();
+            winner = true;
+        } else if (board[0] !== undefined && board[0] === board[3] && board[0] === board[6]){ //left column
+            colorLine(0, 3, 6);
+            resetBoard();
+            winner = true;
+        } else if (board[2] !== undefined && board[2] === board[5] && board[2] === board[8]){ //right column
+            colorLine(2, 5, 8);
+            resetBoard();
+            winner = true;
+        } else if (board[1] !== undefined && board[1] === board[4] && board[1] === board[7]){ //middle column
+            colorLine(1, 4, 7);
+            resetBoard();
+            winner = true;
+        } else if (board[0] !== undefined && board[0] === board[4] && board[0] === board[8]){ //diagonal \
+            colorLine(0, 4, 8);
+            resetBoard();
+            winner = true;
+        } else if (board[2] !== undefined && board[2] === board[4] && board[2] === board[6]){ //diagonal /
+            colorLine(2, 4, 6);
+            resetBoard();
+            winner = true;
+        } else if (board.includes(undefined) === false) {
+            resetBoard();
+            draw = true;
+        }
     };
+
+    const colorLine = (position1, position2, position3) => {
+        const cell1 = document.getElementById('cell' + position1);
+        const cell2 = document.getElementById('cell' + position2);
+        const cell3 = document.getElementById('cell' + position3);
+        cell1.style.color = 'green';
+        cell2.style.color = 'green';
+        cell3.style.color = 'green';
+    }
 
     updateBoard();
 })();
